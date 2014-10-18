fd53aa4fa877df14f99fb08c586097973877cc12
markhovskiy <olexiy.markhovsky@gmail.com>
2014-10-18 19:04:45 +0200

    keep context, handle empty and not found routes, normalize hash

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-var Router = function (routes) {
+var Router = function (routes, context) {
     this.routes = routes;
-    this.on_hash_change();
-    window.addEventListener('hashchange', this.on_hash_change.bind(this));
+    this.context = context;
+    this.notFound = null;
 };
 
-Router.prototype.on_hash_change = function () {
-    var matches = null;
+Router.prototype.start = function () {
+    window.addEventListener('hashchange', this._go.bind(this));
+    this._go();
+};
+
+Router.prototype._go = function () {
+    var normalized = location.hash.replace(/^#|\/$/g, ''),
+        matches = null;
+
     for (var pattern in this.routes) {
-        matches = location.hash.slice(1).match(new RegExp(pattern));
+        matches = normalized.match(new RegExp('^' + pattern + '$'));
         if (matches) {
-            this.routes[pattern].apply(null, matches.slice(1));
+            this.routes[pattern].apply(this.context, matches.slice(1));
+            return;
         }
+    }
+
+    if (typeof this.notFound === 'function') {
+        this.notFound.apply(this.context);
     }
 };
