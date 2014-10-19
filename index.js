dce08b55916397572a05c72ada42363369f6af25
markhovskiy <olexiy.markhovsky@gmail.com>
2014-10-19 21:54:51 +0200

    add amd registration

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-var Router = function (routes, context) {
-    this.routes = routes;
-    this.context = context;
-    this.notFound = null;
-};
+(function () {
+    var Router = function (routes, context) {
+        this.routes = routes;
+        this.context = context;
+        this.notFound = null;
+    };
 
-Router.prototype.start = function () {
-    window.addEventListener('hashchange', this._go.bind(this));
-    this._go();
-    return this;
-};
+    Router.prototype.start = function () {
+        window.addEventListener('hashchange', this._go.bind(this));
+        this._go();
+        return this;
+    };
 
-Router.prototype._go = function () {
-    var normalized = location.hash.replace(/^#|\/$/g, ''),
-        matches = null;
+    Router.prototype._go = function () {
+        var normalized = location.hash.replace(/^#|\/$/g, ''),
+            matches = null;
 
-    for (var pattern in this.routes) {
-        matches = normalized.match(new RegExp('^' + pattern + '$'));
-        if (matches) {
-            this.routes[pattern].apply(this.context, matches.slice(1));
-            return;
+        for (var pattern in this.routes) {
+            matches = normalized.match(new RegExp('^' + pattern + '$'));
+            if (matches) {
+                this.routes[pattern].apply(this.context, matches.slice(1));
+                return;
+            }
         }
-    }
 
-    if (typeof this.notFound === 'function') {
-        this.notFound.apply(this.context);
-    }
-};
+        if (typeof this.notFound === 'function') {
+            this.notFound.apply(this.context);
+        }
+    };
 
-// Node export, to run mocha tests
-if (typeof module !== 'undefined' &&
-    typeof module.exports !== 'undefined') {
-    module.exports = Router;
-}
+    if (typeof module !== 'undefined' &&
+        typeof module.exports !== 'undefined') {
+        // node (to run mocha tests)
+        module.exports = Router;
+    } else if (typeof define === 'function' && define.amd) {
+        // requirejs
+        define([], function () { return Router; });
+    } else {
+        // browser
+        window.Router = Router;
+    }
+})();
